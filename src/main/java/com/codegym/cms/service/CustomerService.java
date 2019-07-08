147673cd14f24dfbd5e5a5382e276f5aba3186ec
HieuThao <you@example.com>
2019-07-08 16:51:51 +0700

    bt4

--- src/main/java/com/codegym/cms/service/CustomerService.java
+++ src/main/java/com/codegym/cms/service/CustomerService.java
@@ -1,10 +1,14 @@
 package com.codegym.cms.service;
 
 import com.codegym.cms.model.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 
 public interface CustomerService {
-    Iterable<Customer> findAll();
+    Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable);
+
+    Page<Customer> findAll(Pageable pageable);
 
     Customer findById(Long id);
 
--- src/main/java/com/codegym/cms/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/codegym/cms/service/impl/CustomerServiceImpl.java
@@ -4,6 +4,8 @@ import com.codegym.cms.model.Customer;
 import com.codegym.cms.repository.CustomerRepository;
 import com.codegym.cms.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -13,8 +15,13 @@ public class CustomerServiceImpl implements CustomerService{
     private CustomerRepository customerRepository;
 
     @Override
-    public Iterable<Customer> findAll() {
-        return customerRepository.findAll();
+    public Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable) {
+        return customerRepository.findAllByFirstNameContaining(firstname, pageable);
+    }
+
+    @Override
+    public Page<Customer> findAll(Pageable pageable) {
+        return customerRepository.findAll(pageable);
     }
 
     @Override
