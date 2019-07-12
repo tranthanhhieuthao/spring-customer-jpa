975933131f834c70cffaea5b4339816036478b36
HieuThao <you@example.com>
2019-07-12 17:40:39 +0700

    bt6

--- src/main/java/com/codegym/cms/controller/CustomerController.java
+++ src/main/java/com/codegym/cms/controller/CustomerController.java
@@ -7,6 +7,7 @@ import com.codegym.cms.service.ProvinceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -48,7 +49,7 @@ public class CustomerController {
     }
 
     @GetMapping("/customers")
-    public ModelAndView listCustomers(@RequestParam("s") Optional<String> s, Pageable pageable){
+    public ModelAndView listCustomers(@RequestParam("s") Optional<String> s,@PageableDefault(size=3) Pageable pageable){
         Page<Customer> customers;
         if(s.isPresent()){
             customers = customerService.findAllByFirstNameContaining(s.get(), pageable);
